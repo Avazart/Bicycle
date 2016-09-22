64fa57708f842b754868afa388d73ecfa7fbdd1f
Avazart <avazart7@gmail.com>
2016-09-22 18:37:01 +0300

    upgrade example

--- Examples/main_Process.cpp
+++ Examples/main_Process.cpp
@@ -12,56 +12,52 @@ int main()
 
   try
   {
-     Bi::Process process;
+    Bi::Process process;
 
-     wstring progPath= L"ping";
-     wstring parms= L"google.com";
-     wstring workDir= L".";
+    wstring progPath= L"ping";
+    wstring parms= L"google.com";
+    wstring workDir= L".";
 
-     process.setReadTimeOut(30000);
-     process.setCurrentDir(workDir);
-     process.setInheritHandle(true);
-     process.start(L"",progPath+L" "+parms);
+    process.setReadTimeOut(30000);
+    process.setCurrentDir(workDir);
+    process.setInheritHandle(true);
+    process.start(L"",progPath+L" "+parms);
 
-     Bi::ulong error;
-     Bi::IOStream stream(&process);
-     do
-     {
-       string line;
-       if(stream.readLine(line,error) != 0 )
-       {
-         cout<<"\""<<line<<"\""<<endl;
-       }
-     }
-     while(!error);
+    Bi::ulong error;
+    Bi::IOStream stream(&process);
+    do
+    {
+      string line;
+      if(stream.readLine(line,error) != 0 )
+      {
+        cout<<"\""<<line<<"\""<<endl;
+      }
+    }
+    while(!error);
 
-     /*
-     ulong exitCode= 0;
-     if(process.waitForFinished(exitCode,30000))
-     {
-       std::cout<<"Процесс завершился кодом "<<exitCode<<std::endl;
-     }
-     */
-
-     if(error==Bi::ProcessError::WaitTimeOut)
-     {
-         Bi::Console::setTextAttr(Bi::ConsoleColor::blue);
-         cout<<"Timeout!"<<endl;
-     }
-     else
-     {
-         Bi::Console::changeCp(Bi::Win1251);
-         Bi::Console::setTextAttr(Bi::ConsoleColor::red);
-         cerr<<Bi::formatMessage(error)<<endl;
-     }
-
+    if(error==Bi::ProcessError::WaitTimeOut) // Read TimeOut
+    {
+      Bi::Console::setTextAttr(Bi::ConsoleColor::blue);
+      cout<<"Timeout!"<<endl;
+    }
+    else if(error==Bi::ProcessError::Broken)// Pipe closed (App Finished)
+    {
+      Bi::Console::setTextAttr(Bi::ConsoleColor::yellow);
+      cout<<"Exit code: "<< process.exitCode() <<endl;
+    }
+    else// Error
+    {
+      Bi::Console::changeCp(Bi::Win1251);
+      Bi::Console::setTextAttr(Bi::ConsoleColor::red);
+      cerr<<Bi::formatMessage(error)<<endl;
+    }
   }
   catch(const Bi::SystemException& e)
   {
-     cerr<< e.what()<<endl;
+    cerr<< e.what()<<endl;
   }
 
-	getchar();
+  getchar();
   return 0;
 }
 
